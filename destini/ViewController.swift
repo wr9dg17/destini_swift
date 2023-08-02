0c3a5312df4e5d6c9c2cc8b3b5df3fb3a09e9d6b
Farid D. Hamzayev <Farid.Hamzayev@kapitalbank.az>
2023-08-02 15:42:03 +0400

    Initial Commit

---
+++ destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  destini
+//
+//  Created by Farid Hamzayev on 02.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
